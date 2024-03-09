fab68002dbde12a0f2d38e77180c87a73eb6d304
gloubi-boulga <guillaumehx@outlook.com>
2024-03-09 13:54:40 +0100

    grunt: init

---
+++ chapter-0/exercise-1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("chapter-0 exercise-1\n");
+    return 0;
+}
---
+++ chapter-1/exercise-1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("chapter-1 exercise-1\n");
+    return 0;
+}
