7ce478bc0b01a2602662b54d36fe111963d4733c
joepav <jpavlisko@protonmail.com>
2024-03-24 21:29:03 +0100

    feat: allow non-alpha chars, reorder funcs to prevent separate declaration/implementations; eliminate an unreachable LOC

--- chapter-1/sinfo054-groupe08-travail1.c
+++ chapter-1/sinfo054-groupe08-travail1.c
@@ -20,24 +20,6 @@
 char buffer[MAX_BUFFER_VALUE];
 int bufferIndex = 0;
 
-void getCharacterInput();
-void processAlarmSignal();
-
-void handler(int signum) {
-    if (signum == SIGALRM) {
-        processAlarmSignal();
-        alarm(ALARM_INTERVAL);
-    }
-}
-
-int main(int argc, char *argv[]) {
-    printf("Program started, taking at most %d characters :\n", MAX_BUFFER_VALUE);
-    signal(SIGALRM , handler);
-    alarm(ALARM_INTERVAL);
-    getCharacterInput();
-    return EXIT_SUCCESS;
-}
-
 void processAlarmSignal() {
     if (bufferIndex == 0) {
         printf("Nothing to process, terminate the program\n");
@@ -46,23 +28,40 @@ void processAlarmSignal() {
 
     printf ("Processing buffer : ");
     for (int i = 0; i < bufferIndex; i++) {
-        printf("%c ", toupper(buffer[i]));
+        int character = buffer[i];
+        if (character < 123 && character >= 97) {
+            printf("%c ", toupper(buffer[i]));
+        } else {
+            printf("%c ", buffer[i]);
+        }
         buffer[i] = '\0'; // Once printed reset to default
     }
     bufferIndex = 0;
     printf("\n");
 }
 
+void handler(int signum) {
+    if (signum == SIGALRM) {
+        processAlarmSignal();
+        alarm(ALARM_INTERVAL);
+    }
+}
+
 void getCharacterInput() {
     while (1) {
         if (bufferIndex < MAX_BUFFER_VALUE) {
             char inputChar = getchar();
-            if (isalpha(inputChar)) {
-                buffer[bufferIndex++] = inputChar;
-            }
+            buffer[bufferIndex++] = inputChar;
         } else {
             printf("Buffer capacity reached. Awaiting next signal to clear the buffer\n");
             pause();
         }
     }
+}
+
+int main(int argc, char *argv[]) {
+    printf("Program started, taking at most %d characters :\n", MAX_BUFFER_VALUE);
+    signal(SIGALRM , handler);
+    alarm(ALARM_INTERVAL);
+    getCharacterInput();
 }
